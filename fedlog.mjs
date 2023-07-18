e625a50cbd4b7456251c09e5a7a3b3daeb035bc3
Jonas Almeida <jalmeida@mathbiol.org>
2023-07-18 16:30:11 -0400

    Update fedlog.mjs

--- fedlog.mjs
+++ fedlog.mjs
@@ -1,1 +1,12 @@
-console.log(Date())
+console.log(`fedlog.mjs loaded at /n${Date()}`);
+
+import plotly from 'https://cdn.jsdelivr.net/npm/plotly@1.0.6/+esm'
+import {epibox as epibox} from 'https://episphere.github.io/epibox/export.js'
+//let epibox = (await import('https://episphere.github.io/epibox/export.js')).epibox
+const loaded = Date()
+
+export{
+    loaded,
+    epibox,
+    plotly
+}
