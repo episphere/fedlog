f3a5b6a889acc7ef9d67716fec6b5e9f7bf0d74b
Jonas Almeida <jalmeida@mathbiol.org>
2023-07-18 16:53:11 -0400

    Update fedlog.mjs

--- fedlog.mjs
+++ fedlog.mjs
@@ -3,10 +3,15 @@ console.log(`fedlog.mjs loaded at /n${Date()}`);
 import plotly from 'https://cdn.jsdelivr.net/npm/plotly@1.0.6/+esm'
 import {epibox as epibox} from 'https://episphere.github.io/epibox/export.js'
 //let epibox = (await import('https://episphere.github.io/epibox/export.js')).epibox
+//import fminsearch from 'https://jonasalmeida.github.io/fminsearch/fminsearch.mjs'
+//import fminsearch from './fminsearch/fminsearch.mjs'
+//function fminsearch = import('http:8000/fminsearch/fminsearch.mjs')
+
 const loaded = Date()
 
 export{
     loaded,
     epibox,
-    plotly
+    plotly,
+    //fminsearch
 }
